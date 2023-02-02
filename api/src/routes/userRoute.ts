b0b07e847b1578eedfa5796dc44b19a1c0878ac9
francoag98 <francoaglieri@hotmail.com>
2023-02-01 21:49:32 -0300

    route

--- api/src/routes/userRoute.ts
+++ api/src/routes/userRoute.ts
@@ -1,7 +1,20 @@
 import { Request, Response } from "express";
 import {Router} from "express";
+import { newUser } from "../Controllers/Users/UserController";
 const route = Router();
 
-
+route.post("/users", async (req: Request, res: Response) =>{
+    const { Body } = req.body
+    try {
+        const userFind = await newUser(Body)
+        if(userFind){
+            res.status(400).send("User already exist")
+        }else {
+            res.status(200).send("user created")
+        }
+    } catch (error) {
+        res.status(400).send({ error })
+    }
+})
 
 export default route
