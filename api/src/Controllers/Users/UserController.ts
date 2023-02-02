1f43abbcbec1d4d5180b25a1f70f28c2bc6da50b
Franco Aglieri <francoaglieri@hotmail.com>
2023-02-02 15:10:04 -0300

    controllers and routes

--- api/src/Controllers/Users/UserController.ts
+++ api/src/Controllers/Users/UserController.ts
@@ -5,11 +5,24 @@ import User from "../../models/User";
 export const newUser = async (Body: user) => {
   if (!Body.Email || !Body.Name || !Body.LastName || !Body.Password)
     throw Error("Info Missing");
-  const userExist = await User.findOne({ Email: Body.Email });
-  if (userExist) {
-    throw Error("User already exist");
-  } else {
-    let newUsers = await User.create({ ...Body });
-    return newUsers;
+  if (Body.Email) {
+    const userExist = await User.findOne({ Email: Body.Email });
+    if (userExist) {
+      throw Error("User already exist");
+    }
   }
+  let newUsers = await User.create({ ...Body });
+  return newUsers;
+};
+
+export const getUser = async (id: String) => {
+  if (!id) throw Error("info missing");
+  const userFind = await User.findOne({ _id: id });
+  if (!userFind) throw Error("User with that id not exist");
+  return userFind;
+};
+
+export const getAllUsers = async () => {
+  const users = await User.find();
+  return users;
 };
--- api/src/routes/userRoute.ts
+++ api/src/routes/userRoute.ts
@@ -1,20 +1,43 @@
 import { Request, Response } from "express";
-import {Router} from "express";
-import { newUser } from "../Controllers/Users/UserController";
+import { Router } from "express";
+import {
+  getAllUsers,
+  getUser,
+  newUser,
+} from "../Controllers/Users/UserController";
 const route = Router();
 
-route.post("/users", async (req: Request, res: Response) =>{
-    const { Body } = req.body
-    try {
-        const userFind = await newUser(Body)
-        if(userFind){
-            res.status(400).send("User already exist")
-        }else {
-            res.status(200).send("user created")
-        }
-    } catch (error) {
-        res.status(400).send({ error })
+route.post("/users", async (req: Request, res: Response) => {
+  const Body = req.body;
+  console.log(Body);
+  try {
+    console.log("estoy aca");
+    const newPerson = await newUser(Body);
+    if (newPerson) {
+      res.status(200).send(newPerson);
     }
-})
+  } catch (err: any) {
+    res.status(400).send(err.message);
+  }
+});
 
-export default route
+route.get("/users", async (req: Request, res: Response) => {
+  try {
+    const users = await getAllUsers();
+    res.status(200).send(users);
+  } catch (err: any) {
+    res.status(400).send({ err: err.message });
+  }
+});
+
+route.get("/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await getUser(id);
+    res.status(200).send(user);
+  } catch (err: any) {
+    res.status(400).send({ err: err.message });
+  }
+});
+
+export default route;
